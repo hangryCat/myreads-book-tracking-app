4af984625336c323ed99ea79ae1033698c758c72
Meo Huynh <mayoh727@gmail.com>
2018-09-28 16:26:59 -0700

    test: Add componentDidMount() to getAll() from BooksAPI

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -2,10 +2,20 @@ import React from 'react';
 // This component provides declarative, accessible navigation around the app
 // The TO property within the LINK tag will be the path we want to link to
 import { Link } from 'react-router-dom';
+// Imports everything from BooksAPI
+import * as BooksAPI from '../../BooksAPI'
 // Shelf is one level up the directory so use ../
 import Shelf from '../Shelf';
 
 class MainPage extends React.Component {
+  // This method will help load all currently reading books
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then(resp => {
+        console.log(resp);
+      });
+  }
+
   render () {
     return (
       <div className="list-books">
