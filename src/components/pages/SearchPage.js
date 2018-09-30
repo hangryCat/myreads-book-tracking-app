3be1221bce4a65d11df70b025686d00ce7ba3ec5
Meo Huynh <mayoh727@gmail.com>
2018-09-30 15:22:56 -0700

    feat: Copy updateBook func

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import * as BooksAPI from '../../BooksAPI'
 // This component provides declarative, accessible navigation around the app
 // The TO property within the LINK tag will be the path we want to link to
 import { Link } from 'react-router-dom';
 
 class SearchPage extends React.Component {
+  // To have some starting books state (read, want to read, etc.)
+  constructor(props) {
+    // The super keyword is used to access and call functions on an object's parent
+    // When used in a constructor,
+      // the super keyword appears alone
+      // and must be used before the this keyword is used
+    super(props);
+    this.state = {
+      books: [],
+      results: []
+    }
+  }
+  // This method will help load all currently reading books
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then(resp => {
+        console.log(resp);
+        // Add the resp(onse) to component state which is an array of bookshelf
+        // When setState is called, it'll force a rerender; render() runs again
+        this.setState({ books: resp });
+      });
+  }
+
+  updateBook = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+    .then(resp => {
+      book.shelf = shelf;
+      this.setState(state => ({
+        books: state.books.filter(bk => bk.id !== book.id).concat([book])
+      }));
+    });
+  }
+
   render () {
     return (
       <div className="search-books">
